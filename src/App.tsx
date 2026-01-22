ad324a95365c1f5940f9c82720851d146a415ba6
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2026-01-22 15:33:41 +0100

    v9

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import style from './App.module.scss'
 import './App.css'
 
 function App() {
-const productUrl = `https://official-joke-api.appspot.com/random_joke`
+const JokesUrl = `https://official-joke-api.appspot.com/random_joke`
+const [JokeData, setJokeData] = useState()
+
+const fetchNewJoke = () => {
+  fetch(JokesUrl)
+  .then((res) => res.json())
+  .then((data) => setJokeData(data))
+}
 
 useEffect (()=>{
-  fetch(productUrl)
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-
+  fetchNewJoke()
 },[])
+
+console.log(JokeData)
   return (
     <>
+    <main className={style.mainStyle}>
+      <section className={style.section}>
+        <p>{JokeData?.setup}</p>
+        <p>{JokeData?.punchline}</p>
+        <button onClick={fetchNewJoke}>Next</button>
+      </section>
+    </main>
      
     </>
   )
